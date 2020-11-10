6cfdc016c59baafa32b0c87ca28d36cd83bbbdd3
Максим Вахтель <bitar1337@gmail.com>
2020-11-11 00:35:39 +0400

    some fix

--- src/pages/pay/[payOperatorID].tsx
+++ src/pages/pay/[payOperatorID].tsx
@@ -63,7 +63,8 @@ const PaymentPage = ({operatorData}: PaymentProps) => {
             `${PAGE_URL}/api/pay`, {
                 method: 'POST',
                 headers: {
-                    'Content-Type': 'application/json'
+                    'Content-Type': 'application/json',
+                    'Access-Control-Allow-Origin': '*'
                 },
                 body: JSON.stringify(reqBody)
             });
