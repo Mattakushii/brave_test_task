48048db01bf30f1f9ed81e2b34071706347425c5
Максим Вахтель <bitar1337@gmail.com>
2020-11-10 23:56:20 +0400

    prepare to deploy

--- .next/static/development/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/pay/[payOperatorID]":["static\u002Fchunks\u002Fpages\u002Fpay\u002F[payOperatorID].js"],sortedPages:["\u002F","\u002F_app","\u002F_error","\u002Fpay\u002F[payOperatorID]"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
