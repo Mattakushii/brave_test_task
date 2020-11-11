e5c7e0711111f24be21287f571352c2da66c601a
Максим Вахтель <bitar1337@gmail.com>
2020-11-11 12:54:54 +0400

    final preparings

--- src/pages/api/pay/index.ts
+++ src/pages/api/pay/index.ts
@@ -1,7 +1,6 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 
 export default function payHandler(req : NextApiRequest, res: NextApiResponse) {
-    console.log(req.method);
     res.setHeader('Access-Control-Allow-Origin', '*');
     const randRes =  randomInteger(0, 1);
     if(randRes) {
@@ -19,39 +18,6 @@ export default function payHandler(req : NextApiRequest, res: NextApiResponse) {
             });
         }, 2000)
     }
-    // switch (req.method) {
-    //     case 'POST':
-    //         if(req.body.phoneNumber && req.body.payment && req.body.operatorName) {
-    //             const randRes =  randomInteger(0, 1);
-    //             if(randRes) {
-    //                 setTimeout(() => {
-    //                     res.status(200).json({
-    //                         result: true,
-    //                         message: `Успешно!`
-    //                     });
-    //                 }, 2000);
-    //             } else {
-    //                 setTimeout(() => {
-    //                     res.status(404).json({
-    //                         result: false,
-    //                         message: `Кажется, что то пошло не так..(`
-    //                     });
-    //                 }, 2000)
-    //             }
-    //         } else {
-    //             res.status(404).json({
-    //                 result: false,
-    //                 message: `Invalid data`
-    //             });
-    //         }
-    //         break;
-    //     default: {
-    //         res.status(405).json({
-    //             result: false,
-    //             message: `${req.method} not allowed`});
-    //         break;
-    //     }
-    // }
 }
 
 function randomInteger(min : number, max : number) {
